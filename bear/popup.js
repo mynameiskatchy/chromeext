d46780f7212ba6d110f2b556196ae83b36f12275
mynameiskatchy <karchietrinh@gmail.com>
2021-08-12 13:56:22 -0400

    Count bears on single page (9:47)

--- bear/popup.js
+++ bear/popup.js
@@ -5,7 +5,13 @@ document.addEventListener(
     function onclick() {
       chrome.tabs.query({ currentWindow: true, active: true },
         function (tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, 'hi');
+          chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount);
         })
+    }
+
+    function setCount(res) {
+      const div = document.createElement('div')
+      div.textContent = `${res.count} bears`
+      document.body.appendChild(div)
     }
   }, false)
